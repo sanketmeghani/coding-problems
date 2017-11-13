07b52bdf620228547b90aad67e1a502a2538f68e
sanketmeghani <sanket.meghani@gmail.com>
2017-11-13 15:38:49 +0530

    Adding additional tests

--- problems/subtree-sum/src/test/java/dev/sanket/tree/BinaryTreeTest.java
+++ problems/subtree-sum/src/test/java/dev/sanket/tree/BinaryTreeTest.java
@@ -1,10 +1,11 @@
 package dev.sanket.tree;
 
-import java.util.List;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.Test;
 
@@ -14,12 +15,40 @@ public class BinaryTreeTest {
     public void shouldReturnThreeNodes() {
 
         int[] values = { 3, 4, 0, 1, 2, 0, 7 };
-
+        int target = 7;
+        
         BinaryTree binaryTree = BinaryTree.fromArray(values);
-        List<Node> nodes = binaryTree.findNodes(7);
+        List<Node> nodes = binaryTree.findNodes(target);
 
         assertNotNull(nodes);
         assertFalse("No nodes with given target found", nodes.isEmpty());
         assertEquals("Unexpected number of nodes found", 3, nodes.size());
+    }
+    
+    @Test
+    public void shouldReturnOneNodes() {
+
+        int[] values = { 3, 4, 0, 1, 2, 0, 7 };
+        int target = 2;
+        
+        BinaryTree binaryTree = BinaryTree.fromArray(values);
+        List<Node> nodes = binaryTree.findNodes(target);
+
+        assertNotNull(nodes);
+        assertFalse("No nodes with given target found", nodes.isEmpty());
+        assertEquals("Unexpected number of nodes found", 1, nodes.size());
+    }
+    
+    @Test
+    public void shouldNotReturnAnyNode() {
+        
+        int[] values = { 3, 4, 0, 1, 2, 0, 7 };
+        int target = 13;
+        
+        BinaryTree binaryTree = BinaryTree.fromArray(values);
+        List<Node> nodes = binaryTree.findNodes(target);
+
+        assertNotNull(nodes);
+        assertTrue("No nodes with given target found", nodes.isEmpty());
     }
 }

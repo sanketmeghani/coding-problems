3b82711973f94427c42d988e5c502c54b9765c3e
sanketmeghani <sanket.meghani@gmail.com>
2017-11-22 14:48:19 +0530

    Correcting test message

--- problems/rotated-array/src/test/java/dev/sanket/rotatedarray/RotatedArrayTest.java
+++ problems/rotated-array/src/test/java/dev/sanket/rotatedarray/RotatedArrayTest.java
@@ -18,7 +18,7 @@ public class RotatedArrayTest {
 
         int[] input = { 9, 15, 23, 50, 100, 1, 4, 7 };
 
-        assertEquals("Pivot found for non-rotated array", 4, RotatedArray.findPivot(input));
+        assertEquals("Incorrect pivot found for rotated array", 4, RotatedArray.findPivot(input));
     }
 
     @Test
@@ -26,6 +26,6 @@ public class RotatedArrayTest {
 
         int[] input = { 7, 4, 1, 100, 50, 23, 15, 9 };
 
-        assertEquals("Pivot found for non-rotated array", 2, RotatedArray.findPivot(input));
+        assertEquals("Incorrect pivot found for rotated array", 2, RotatedArray.findPivot(input));
     }
 }

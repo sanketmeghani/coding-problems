30c67768d9580ea9d0f69f5a463674cf7664e898
sanketmeghani <sanket.meghani@gmail.com>
2018-01-15 23:26:00 +0530

    Adding implementation stub

---
+++ problems/min-heap/src/main/java/dev/sanket/minheap/MinHeap.java
@@ -0,0 +1,20 @@
+package dev.sanket.minheap;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MinHeap {
+
+    private List<Integer> nodes = new LinkedList<>();
+    
+    public void addNode() {
+    }
+    
+    public Integer peek() {
+        return null;
+    }
+    
+    public Integer poll() {
+        return null;
+    }
+}

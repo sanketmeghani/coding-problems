fb0a490f94aa164a2505ed8564ec4953e806cec9
sanketmeghani <sanket.meghani@gmail.com>
2018-01-21 20:06:00 +0530

    Implementing solution

--- problems/connected-cells/src/main/java/dev/sanket/ConnectedCells.java
+++ problems/connected-cells/src/main/java/dev/sanket/ConnectedCells.java
@@ -1,8 +1,28 @@
 package dev.sanket;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class ConnectedCells {
 
     public static int connectedCell(int[][] matrix) {
-        return -1;
+
+        for (int i = matrix.length - 1; i >= 0; i--) {
+            for (int j = matrix[i].length - 1; j >= 0; j--) {
+                if (matrix[i][j] == 1) {
+                    matrix[i][j] += rightAdjascentRegions(matrix, i, j) + bottomAdjascentRegions(matrix, i, j);
+                }
+            }
+        }
+
+        return Stream.of(matrix).flatMapToInt(IntStream::of).max().getAsInt();
+    }
+
+    private static int rightAdjascentRegions(int[][] matrix, int i, int j) {
+        return (j < matrix[i].length - 1) ? matrix[i][j + 1] : 0;
+    }
+
+    private static int bottomAdjascentRegions(int[][] matrix, int i, int j) {
+        return (i < matrix.length - 1) ? matrix[i + 1][j] : 0;
     }
 }

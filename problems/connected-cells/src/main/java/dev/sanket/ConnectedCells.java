f4a6f0bfd5b2aa02e6e0809698b2f18fee2f3e90
sanketmeghani <sanket.meghani@gmail.com>
2018-01-22 11:45:17 +0530

    [ci skip] Considering full square scenario

--- problems/connected-cells/src/main/java/dev/sanket/ConnectedCells.java
+++ problems/connected-cells/src/main/java/dev/sanket/ConnectedCells.java
@@ -10,12 +10,18 @@ public class ConnectedCells {
         for (int i = matrix.length - 1; i >= 0; i--) {
             for (int j = matrix[i].length - 1; j >= 0; j--) {
                 if (matrix[i][j] == 1) {
-                    matrix[i][j] += rightAdjascentRegions(matrix, i, j) + bottomAdjascentRegions(matrix, i, j);
+                    matrix[i][j] += rightAdjascentRegions(matrix, i, j) + bottomAdjascentRegions(matrix, i, j)
+                            - removeDuplicateAdditions(matrix, i, j);
                 }
             }
         }
 
         return Stream.of(matrix).flatMapToInt(IntStream::of).max().getAsInt();
+    }
+
+    private static int removeDuplicateAdditions(int[][] matrix, int i, int j) {
+        return ((i < matrix.length - 1) && (j < matrix[i].length - 1) && (matrix[i + 1][j] != 0)
+                && (matrix[i][j + 1] != 0)) ? matrix[i + 1][j + 1] : 0;
     }
 
     private static int rightAdjascentRegions(int[][] matrix, int i, int j) {

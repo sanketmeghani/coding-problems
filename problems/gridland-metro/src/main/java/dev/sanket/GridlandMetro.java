324c55647660852cfb00636bc3f79f48b7bcace8
sanketmeghani <sanket.meghani@gmail.com>
2018-02-10 23:25:09 +0530

    Code refactoring and adding more tests

--- problems/gridland-metro/src/main/java/dev/sanket/GridlandMetro.java
+++ problems/gridland-metro/src/main/java/dev/sanket/GridlandMetro.java
@@ -5,53 +5,60 @@ import java.util.Comparator;
 
 public class GridlandMetro {
 
-    private static final class TrackComparator implements Comparator<long[]> {
+    public static long findNoOfLampposts(long n, long m, long k, long[][] tracks) {
 
-        @Override
-        public int compare(long[] o1, long[] o2) {
-
-            if (o1[0] < o2[0]) {
-                return -1;
-            }
-
-            if (o1[0] == o2[0]) {
-                return o1[1] <= o2[1] ? -1 : 1;
-            }
-
-            return 1;
-        }
-    }
-
-    public static long findNoOfLampposts(long n, long m, long k, long[][] track) {
-
-        Arrays.sort(track, new TrackComparator());
+        Arrays.sort(tracks, new TrackComparator());
 
         long count = 0;
 
         for (int i = 0; i < k; i++) {
 
             int j = i + 1;
-            while ((j < k) && (track[i][0] == track[j][0]) && (track[i][2] >= track[j][1] - 1)) {
-                track[i][2] = Math.max(track[i][2], track[j][2]);
+            
+            while ((j < k) && (rowOf(tracks[i]) == rowOf(tracks[j]))
+                    && (terminationPointOf(tracks[i]) >= startingPointOf(tracks[j]) - 1)) {
+                
+                mergeTracks(tracks, i, j);
                 j++;
             }
 
-            count = count + (track[i][2] - track[i][1] + 1);
+            count = count + (tracks[i][2] - tracks[i][1] + 1);
             i = j - 1;
         }
 
         return (m * n) - count;
     }
-    
+
+    private static void mergeTracks(long[][] tracks, int i, int j) {
+        tracks[i][2] = Math.max(terminationPointOf(tracks[i]), terminationPointOf(tracks[j]));
+    }
+
     private static long rowOf(long[] track) {
         return track[0];
     }
-    
+
     private static long startingPointOf(long[] track) {
         return track[1];
     }
-    
+
     private static long terminationPointOf(long[] track) {
         return track[2];
+    }
+    
+    private static final class TrackComparator implements Comparator<long[]> {
+
+        @Override
+        public int compare(long[] firstTrack, long[] secondTrack) {
+
+            if (firstTrack[0] < secondTrack[0]) {
+                return -1;
+            }
+
+            if (firstTrack[0] == secondTrack[0]) {
+                return firstTrack[1] <= secondTrack[1] ? -1 : 1;
+            }
+
+            return 1;
+        }
     }
 }
--- problems/gridland-metro/src/test/java/dev/sanket/GridlandMetroTest.java
+++ problems/gridland-metro/src/test/java/dev/sanket/GridlandMetroTest.java
@@ -16,24 +16,24 @@ import static org.junit.Assert.assertEquals;
 public class GridlandMetroTest {
 
     private long noOfRows;
-    
+
     private long noOfColumns;
-    
+
     private int noOfTracks;
-    
+
     private long[][] tracks;
-    
+
     @Parameter(value = 0)
     public String inputFileName;
 
     @Parameter(value = 1)
     public int noOfLampposts;
-    
+
     @Parameters
     public static Collection<Object[]> testData() {
-        return Arrays.asList(new Object[][] { { "/input1.txt", 9 } });
+        return Arrays.asList(new Object[][] { { "/input1.txt", 9 }, { "/input2.txt", 8 } });
     }
-    
+
     private void readInputs(String fileName) {
 
         try (Scanner in = new Scanner(GridlandMetroTest.class.getResourceAsStream(fileName))) {
@@ -41,9 +41,9 @@ public class GridlandMetroTest {
             noOfRows = in.nextLong();
             noOfColumns = in.nextLong();
             noOfTracks = in.nextInt();
-            
+
             tracks = new long[noOfTracks][3];
-            
+
             for (int i = 0; i < noOfTracks; i++) {
                 for (int j = 0; j < 3; j++) {
                     tracks[i][j] = in.nextLong();
@@ -51,11 +51,11 @@ public class GridlandMetroTest {
             }
         }
     }
-    
+
     @Test
     public void shouldReturnNoOfCells() {
         readInputs(inputFileName);
-        
+
         long result = GridlandMetro.findNoOfLampposts(noOfRows, noOfColumns, noOfTracks, tracks);
 
         assertEquals("Invalid not of lampposts", noOfLampposts, result);

08df184f78ea66af94ebf06985c0ce425f67b2eb
sanketmeghani <sanket.meghani@gmail.com>
2018-05-27 12:51:55 +0530

    Solution implementation

---
+++ problems/counting-game/src/main/java/dev/sanket/CounterGame.java
@@ -0,0 +1,48 @@
+package dev.sanket;
+
+public class CounterGame {
+
+    public static String counterGame(long n) {
+
+        boolean lousiseWins = false;
+
+        while (n != 1) {
+
+            boolean isPowerOfTwo = isPowerOfTwo(n);
+
+            if (isPowerOfTwo) {
+                n = n / 2;
+            } else {
+                n = n - nextLowerPowerOfTwo(n);
+            }
+
+            lousiseWins = !lousiseWins;
+        }
+
+        return lousiseWins ? "Louise" : "Richard";
+    }
+
+    private static long nextLowerPowerOfTwo(long n) {
+
+        long value = 1;
+        long maxPowerOfTwo = 1;
+
+        do {
+
+            boolean isPowerOfTwo = isPowerOfTwo(value);
+
+            if (isPowerOfTwo && value > maxPowerOfTwo) {
+                maxPowerOfTwo = value;
+            }
+
+            value = value << 1;
+
+        } while (value < n);
+
+        return maxPowerOfTwo;
+    }
+
+    private static boolean isPowerOfTwo(long n) {
+        return (n & (n - 1)) == 0 ? true : false;
+    }
+}

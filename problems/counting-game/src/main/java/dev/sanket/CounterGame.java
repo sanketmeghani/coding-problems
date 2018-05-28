987f1c405626afcd034eb6c0b7f28b664588c562
sanketmeghani <sanket.meghani@gmail.com>
2018-05-28 10:36:50 +0530

    Revert "Code refactoring"
    This reverts commit 1e403b962e1ea0a364d8ef79543bc4998d2ced79.

--- problems/counting-game/src/main/java/dev/sanket/CounterGame.java
+++ problems/counting-game/src/main/java/dev/sanket/CounterGame.java
@@ -28,7 +28,15 @@ public class CounterGame {
         long maxPowerOfTwo = 1;
 
         do {
+
+            boolean isPowerOfTwo = isPowerOfTwo(value);
+
+            if (isPowerOfTwo && value > maxPowerOfTwo) {
+                maxPowerOfTwo = value;
+            }
+
             value = value << 1;
+
         } while (value < n);
 
         return maxPowerOfTwo;
